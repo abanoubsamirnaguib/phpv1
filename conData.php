9dfbf5f56e0b6aeff57e9fa317bf4be7f20b7f74
unknown <abanoubsamir@gmail.com>
2021-11-08 04:18:49 +0200

    add database

--- conData.php
+++ conData.php
@@ -1,8 +1,8 @@
 <?php 
- $servername = "localhost";
- $username = "root";
- $password = "";
- $dbname = "monday";
+ $servername = "remotemysql.com";
+ $username = "Iw3EFbqTfb";
+ $password = "PGTYCmwSwd";
+ $dbname = "Iw3EFbqTfb";
  
  // Create connection
  $conn = new mysqli($servername, $username, $password ,$dbname);
